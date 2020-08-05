a35d592dd7a6f85938f36a7084505b287c54263e
samer-habash <sam.habash1984@gmail.com>
2020-08-05 11:12:24 +0300

    Changed POD_NAME to be better suited using kubectl options rather than linux commands

--- Daily-Backup-Jenkins-sam-xps/jobs/producer-CI-CO-BUILD-PUSH-without-K8sPlugin/builds/4/libs/general-jenkins-library/vars/gitCo.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-
-def call(Map gitcoParams) {
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  gitcoParams.branch ]],
-        userRemoteConfigs: [[ url: gitcoParams.url ]]
-    ])
-  }
